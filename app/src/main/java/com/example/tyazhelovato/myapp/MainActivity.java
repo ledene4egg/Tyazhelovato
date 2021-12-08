b2816d8679ba40c9af09a72d89da1ad015d27bc6
Maruse4ka <ledene4egg@rambler.ru>
2021-12-08 12:56:47 +0300

    init

--- app/src/main/java/com/example/tyazhelovato/myapp/MainActivity.java
+++ app/src/main/java/com/example/tyazhelovato/myapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.tyazhelovato.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView myTextView = (TextView) findViewById(R.id.myText);
+        myTextView.setText("Сашеватый какашеватый");
+
+        Button myBtn = (Button) findViewById(R.id.myBtn);
+        myBtn.setText("Марусина кнопочка");
+
+
     }
 }
